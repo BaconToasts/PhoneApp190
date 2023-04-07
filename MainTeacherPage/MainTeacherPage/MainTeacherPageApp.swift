92d8681fd15399cff4f9a2c70e2cf926cda195ee
Ranger-72 <vanguard4forall@gmail.com>
2023-04-06 20:07:04 -0700

    # Jira Task: C1PL-73 # Created Main Teacher view for the GUI. Created seven buttons: (Main,  Requests, Whitelist, Manage Users, Connect Code, Master Control). All buttons are currently non-functional.

---
+++ MainTeacherPage/MainTeacherPage/MainTeacherPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainTeacherPageApp.swift
+//  MainTeacherPage
+//
+//  Created by Eduardo Mariano on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MainTeacherPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTeacherView()
+        }
+    }
+}
