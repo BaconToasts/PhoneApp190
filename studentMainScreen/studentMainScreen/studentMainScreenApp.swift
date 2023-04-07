a6c06a192c0ea09719fa94104dffd1694355bfd9
lukelobo <la.campos2510@gmail.com>
2023-04-06 21:25:19 -0700

    #Jira Task: C1PL-80 I was able to create a main screen that students are able to see once they login with the code for their teacher.

---
+++ studentMainScreen/studentMainScreen/studentMainScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  studentMainScreenApp.swift
+//  studentMainScreen
+//
+//  Created by user1 on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct studentMainScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
