533b995a575b65969187fb199ce6b34aecba5d48
BaconToasts <vlad.puriy@gmail.com>
2023-04-10 21:10:31 -0700

    Connected all contentViews under one Project
    Added transitions from view to view. Buttons actually do stuff now.

---
+++ studentMainScreen/studentMainScreen/Teacher-ConnectCodeView.swift
@@ -0,0 +1,20 @@
+//
+//  Teacher-ConnectCodeView.swift
+//  Project190
+//
+//  Created by Vlad Puriy on 4/10/23.
+//
+
+import SwiftUI
+
+struct Teacher_ConnectCodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Teacher_ConnectCodeView_Previews: PreviewProvider {
+    static var previews: some View {
+        Teacher_ConnectCodeView()
+    }
+}
